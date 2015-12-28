26c020d82f93da8b444202c4ce023a27efe3456b
sravanaganga <sravanaganga.kudaravalli@gmail.com>
2015-12-28 13:15:31 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main
 	{
 		System.out.println("Sravan Jenkins Project Started");
 		First fObject=new First();
+		System.out.println(fObject);
+		System.out.println(fObject.printMessage());
 		System.out.println("Sravan Jenkins Project Ended");
 	}
  }
