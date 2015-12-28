f7ba8db09ba8d5ca3eae01b7eb8da55685aa7418
sravanaganga <sravanaganga.kudaravalli@gmail.com>
2015-12-28 13:00:05 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.lang.*;
 
 public class Main 
  {
+ public void Main()
+ {
+ 	System.out.println("This is an object created by the constructor");
+ }
   public static void main(String args[])
 	{
 		System.out.println("Sravan Jenkins Project");
+		Main object=new Main();
 	}
  }
