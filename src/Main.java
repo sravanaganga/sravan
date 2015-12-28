f5e694590d5b8b7572aa14f9315cd72473b964a5
sravanaganga <sravanaganga.kudaravalli@gmail.com>
2015-12-28 13:13:02 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import java.lang.*;
 
 public class Main 
  {
- static{
- 	System.out.println("This is an object created by the constructor");
- }
   public static void main(String args[])
 	{
-		System.out.println("Sravan Jenkins Project");
+		System.out.println("Sravan Jenkins Project Started");
 		First fObject=new First();
+		System.out.println("Sravan Jenkins Project Ended");
 	}
  }
